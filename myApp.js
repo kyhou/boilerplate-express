01a6089e9a13f919d18f4cee70886db7cd023eac
Kyhou <joelskill@gmail.com>
2022-10-21 00:18:10 -0300

    Serve an HTML File

--- myApp.js
+++ myApp.js
@@ -2,8 +2,10 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 
+absolutePath = __dirname + "/views/index.html";
+
 app.get("/",function(req, res) {
-    res.send("Hello Express");
+    res.sendFile(absolutePath);
 });
 
 
