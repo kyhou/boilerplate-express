64ef6de7f5cd3c5e0b475da05b0f4fb292f4af72
Kyhou <joelskill@gmail.com>
2022-10-23 01:31:13 -0300

    Get Query Parameter Input from the Client

--- myApp.js
+++ myApp.js
@@ -26,11 +26,15 @@ app.get("/now", function (req, res, next) {
     next();
 }, function(req, res){
     res.json({time: req.time});
-})
+});
 
 app.get("/:word/echo", function(req, res){
     res.json({echo: req.params.word});
-})
+});
+
+app.route("/name").get(function(req,res){
+    res.json({name: `${req.query.first} ${req.query.last}`});
+});
 
 
 
